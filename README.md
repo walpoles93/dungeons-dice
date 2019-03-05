ef59132204dfa36ee31f692a237e2b551f02cfcf
Sam Walpole <walpoles93@googlemail.com>
2019-03-05 12:52:42 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+- Install `expo-cli`
+
+```
+sudo npm i -g expo-cli
+```
+
+- Install dependences
+
+```
+npm i
+```
+
+- Run `expo-cli`
+
+```
+npm start
+```
+
+- Download expo client on phone and scan QR code from dev sever
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import { registerRootComponent } from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Hello World!</Text>
+  </View>
+);
+
+registerRootComponent(App);
