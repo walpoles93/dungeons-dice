946777c6122b939bd3ad14363b68d8d8fe0eb027
Sam Walpole <walpoles93@googlemail.com>
2019-03-06 08:07:56 +0000

    convert App to class and view dice roll

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Die from '../helpers/Die';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,10 +17,28 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Hello World!</Text>
-  </View>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.die = new Die(6);
+    this.state = {
+      roll: this.die.roll(),
+    };
+  }
+
+  render() {
+    const { roll } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>{roll}</Text>
+        <Button
+          onPress={() => this.setState({ roll: this.die.roll() })}
+          title="Roll!"
+          accessibilityLabel="Click to roll the die"
+        />
+      </View>
+    );
+  }
+}
 
 registerRootComponent(App);
