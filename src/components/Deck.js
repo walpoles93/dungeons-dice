f2b8e40bbe2f6a217744d1404911cb2d16bcde9a
Sam Walpole <walpoles93@googlemail.com>
2019-03-08 11:11:59 +0000

    create Deck component

---
+++ src/components/Deck.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'flex-start',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+});
+
+const Deck = ({ children }) => (
+  <View style={styles.wrapper}>
+    {children}
+  </View>
+);
+
+Deck.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
+
+export default Deck;
