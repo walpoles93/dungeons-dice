98578fe2b435cb6eca85b96829d3b32bbedb6235
Sam Walpole <walpoles93@googlemail.com>
2019-03-08 12:25:17 +0000

    update card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,37 +7,58 @@ const styles = StyleSheet.create({
   wrapper: {
     borderColor: '#CFD8DC',
     borderWidth: 1,
+    flexDirection: 'column',
     margin: 9,
     width: 150,
+    height: 150,
   },
-  nameText: {
-    backgroundColor: '#039BE5',
-    color: '#FAFAFA',
-    fontSize: 24,
-    padding: 9,
-    textAlign: 'center',
-  },
-  rollText: {
-    backgroundColor: '#FAFAFA',
-    color: '#212121',
-    fontSize: 18,
-    padding: 9,
-    textAlign: 'center',
-  },
-  typeText: {
-    backgroundColor: '#4FC3F7',
-    color: '#FAFAFA',
-    fontSize: 18,
-    padding: 9,
-    textAlign: 'center',
+  contentView: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
   },
 });
 
+const Content = ({
+  backgroundColor,
+  color,
+  children,
+  fontSize,
+  fontWeight,
+}) => (
+  <View style={[styles.contentView, { backgroundColor }]}>
+    <Text style={{ color, fontSize, fontWeight }}>
+      {children}
+    </Text>
+  </View>
+);
+
+Content.propTypes = {
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  fontSize: PropTypes.number,
+  fontWeight: PropTypes.string,
+};
+
+Content.defaultProps = {
+  backgroundColor: '#FAFAFA',
+  color: '#212121',
+  fontSize: 18,
+  fontWeight: '400',
+};
+
 const Card = ({ name, roll, type }) => (
   <View style={styles.wrapper}>
-    <Text style={styles.nameText}>{name}</Text>
-    <Text style={styles.rollText}>{roll ? roll : '-'}</Text>
-    <Text style={styles.typeText}>{type}</Text>
+    <Content backgroundColor="#039BE5" color="#FAFAFA" fontSize={24} >
+      {name}
+    </Content>
+    <Content backgroundColor="#FAFAFA" color="#212121" fontSize={36} fontWeight="bold">
+      {roll ? roll : '-'}
+    </Content>
+    <Content backgroundColor="#4FC3F7" color="#FAFAFA">
+      {type}
+    </Content>
   </View>
 );
 
