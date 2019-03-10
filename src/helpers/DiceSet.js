37ce9208161cdab1dfc7151fce62983a54d1de39
Joshua Atkins <github@joshatkins.co.uk>
2019-03-10 21:07:18 +0000

    add name getter and setter to DiceSet and change removeDie parameter from index to die

--- src/helpers/DiceSet.js
+++ src/helpers/DiceSet.js
@@ -1,36 +1,39 @@
 export default class DiceSet
 {
     // TODO enable dice argument to be either array of dice or single die
-    constructor(dice = [], setName = "Custom dice set")
+    constructor(dice = [], name = "Custom dice set")
     {
         // TODO validate dice argument before assigning property
         this.dice = dice;
-        this.setName = setName;
+        this.name = name;
         Object.seal(this);
+    }
+    
+    get setName() {
+        return this.name;
+    }
+    
+    set setName(name){
+        this.name = name;
     }
     
     addDice(dice)
     {
         // TODO push entire array at once(?)
-        for(var i = 0; i < dice.length; i++)
+        for(let i = 0; i < dice.length; i++)
         {
             this.dice.push(dice[i]);
         }
     }
     
-    removeDie(index)
+    removeDie(die)
     {
-        this.dice.splice(index, 1);
+        this.dice.splice(this.dice.indexOf(die), 1);
     }
-    
-    rename(setName)
-    {
-        this.setName = setName;
-    }
-    
+        
     rollDice()
     {
-        var rolls = [];
+        let rolls = [];
         this.dice.forEach(function(die){
             rolls.push(die.roll());
         });
